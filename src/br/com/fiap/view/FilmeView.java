83809b04539379c7d2e7281503a684ccaaa6bac0
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-09-23 13:32:33 -0300

    Update FilmeView.java

--- src/br/com/fiap/view/FilmeView.java
+++ src/br/com/fiap/view/FilmeView.java
@@ -7,7 +7,7 @@ public class FilmeView{
   public static void main(String[] args){
     String titulo,genero,produtora;
     int codigo, opcao;
-    String[] escolha{"Inserir","Alterar","Excluir","Listar"};
+    String[] escolha = {"Inserir","Alterar","Excluir","Listar"};
     FilmeController filmeController = new FilmeController();
     do{
       try{
@@ -21,14 +21,14 @@ public class FilmeView{
             System.out.println(filmeController.inserirFilme(titulo,genero,produtora));
             break;
           case 1:
-            codigo = JOptionPane.showInputDialog("Digite o código do filme: ");
+            Integer.parseInt(JOptionPane.showInputDialog("Digite o código do filme: "));
             titulo = JOptionPane.showInputDialog("Digite o titulo do filme: ");
             genero = JOptionPane.showInputDialog("Digite o genero do filme: ");
             produtora = JOptionPane.showInputDialog("Digite a produtora do filme: ");
-            System.out.println(filmeController.alterarFilme(codigo,tittulo,genero,produtora));
+            System.out.println(filmeController.alterarFilme(codigo,titulo,genero,produtora));
             break;
           case 2:
-            codigo = JOptionPane.showInputDialog("Digite o código do filme: ");
+            Integer.parseInt(JOptionPane.showInputDialog("Digite o código do filme: "));
             System.out.println(filmeController.excluirFilme(codigo));
             break;
           case 3:
